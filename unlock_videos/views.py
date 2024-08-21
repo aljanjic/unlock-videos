e264c23e1a189df367be21d38b640a948b2396a3
Aljosa <janjic_aljosa@hotmail.com>
2024-08-21 11:25:59 +0200

    Adding delete and patch request

--- unlock_videos/views.py
+++ unlock_videos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse, JsonResponse
 
 from rest_framework.response import Response
@@ -31,11 +31,25 @@ def files(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
+@api_view(['GET', 'DELETE', 'PATCH'])
 # @permission_classes([IsAuthenticated])
 def file(request, file_id):
-    data = MediaFile.objects.get(pk=file_id)
-    serializer = MediaFileSerializer(data)
-    return Response({'file' : serializer.data})
-
+    try:
+        data = MediaFile.objects.get(pk=file_id)
+    except MediaFile.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
     
+    if request.method == 'GET':
+        serializer = MediaFileSerializer(data)
+        return Response({'file' : serializer.data})
+    
+    if request.method == 'PATCH':
+        serializer = MediaFileSerializer(data, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        data.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT) 
