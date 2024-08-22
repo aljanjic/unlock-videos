8ce25ea8a7df87673a93df2e033dff8ab5b33297
Aljosa <janjic_aljosa@hotmail.com>
2024-08-22 19:03:15 +0200

    Fat models, thin views. Auto adding length and size to Model during upload

--- unlock_videos/models.py
+++ unlock_videos/models.py
@@ -1,17 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# class MediaFile(models.Model):
-#     file_name = models.CharField(max_length=1024)
-#     length = models.DurationField()
-#     size = models.IntegerField()
-#     upload_timestamp = models.DateTimeField(auto_now_add=True)
-#     transcript = models.TextField(blank=True)
-#     file = models.FileField(null=True)
+from pydub import AudioSegment
+from moviepy.editor import VideoFileClip
+import os
+from datetime import timedelta
 
-#     def __str__(self) :
-#         return self.file_name
-    
 
 class MediaFile(models.Model):
     FILE_TYPE_CHOICES = [
@@ -21,12 +15,41 @@ class MediaFile(models.Model):
 
     # user = models.ForeignKey(User, on_delete=models.SET_NULL, null = True)
     file_name = models.CharField(max_length=1024)
-    length = models.DurationField()
-    size = models.IntegerField()
+    length = models.DurationField(blank=True, null=True)
+    size = models.IntegerField(blank=True, null=True)
     upload_timestamp = models.DateTimeField(auto_now_add=True)
     transcript = models.TextField(blank=True)
     file = models.FileField(null=True)
-    file_type = models.CharField(max_length=10, choices=FILE_TYPE_CHOICES, default='audio')
+    file_type = models.CharField(max_length=10, choices=FILE_TYPE_CHOICES, blank=True)
 
     def __str__(self):
         return self.file_name
+    
+    def save(self, *args, **kwargs):
+        if self.file:
+
+            extension = os.path.splitext(self.file.name)[1].lower()
+            if extension in ['.mp4', '.avi', '.mov']:
+                self.file_type = 'video'
+            elif extension in ['.mp3', '.wav', '.aac']:
+                self.file_type = 'audio'
+            else:
+                raise ValueError(f"Unsupported file type: {extension}")
+            
+            self.size = self.file.size
+            
+            if self.file_type == 'video':
+                try:
+                    video = VideoFileClip(self.file.path)
+                    self.length = timedelta(seconds=video.duration)  # Convert to timedelta
+                except Exception as e:
+                    raise ValueError(f"Unable to calculate video length: {str(e)}")
+            elif self.file_type == 'audio':
+                try:
+                    audio = AudioSegment.from_file(self.file.path)
+                    self.length = timedelta(seconds=audio.duration_seconds)  # Convert to timedelta
+                except Exception as e:
+                    raise ValueError(f"Unable to calculate audio length: {str(e)}")
+
+
+        super(MediaFile, self).save(*args, **kwargs)
